64aba87226d00a28f1eb1a3ac63fd7f0e89115a8
alvinC <lvinz0790@gmail.com>
2020-12-10 14:36:03 -0800

    fasfd

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // const audio = document.querySelector("audio");
     // audio.play();
 })
-
+//a
 
 const sayHi = (name) => {
     return (
